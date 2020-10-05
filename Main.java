de2281f944ba13094c26e730f55fa4d992b121d3
repl.it user <replituser@example.com>
2020-10-05 23:23:10 +0000

    Added name variable

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+		String name = "adam";
   }
 
 	public static void sayHi(){
 		System.out.println("Hi!");
 	}
+
 }
